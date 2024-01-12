f0ae5e689ca4fb48ef4cae48e3d8432de6562d47
Ridwanur Rashid Siam <siam.forcode01@gmail.com>
2024-01-12 12:46:54 +0600

    connected to database

--- api/index.js
+++ api/index.js
@@ -1,4 +1,13 @@
+import dotenv from 'dotenv';
 import express from 'express';
+import mongoose from 'mongoose';
+dotenv.config();
+
+mongoose.connect(process.env.MONGO).then(() =>{
+    console.log("Conneced to MongoBD");
+}).catch((err) =>{
+    console.log("err")
+});
 
 const app = express();
 app.listen(3000, ()=>{
