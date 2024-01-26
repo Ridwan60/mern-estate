f5d3e14ff5bf9d9d67ee9da31534333c4f37f997
Ridwanur Rashid Siam <siam.forcode01@gmail.com>
2024-01-26 19:01:39 +0600

    created sign in api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,10 +1,9 @@
 import User from "../models/user.model.js";
 import bycryptjs from 'bcryptjs';
 import { errorHandle } from "../utils/error.js";
+import jwt from 'jsonwebtoken';
 
 export const signup = async (req, res, next)=>{
-    
-
     try {
         const {username, email, password } = req.body;
         const hashedPassword = bycryptjs.hashSync(password, 10);
@@ -16,5 +15,22 @@ export const signup = async (req, res, next)=>{
         // next(errorHandle(550, 'Error from the function'))
         next(error)
     }
-
 };
+
+export const signin = async (req, res, next) => {
+    const {email, password} = req.body;
+    try {
+        const validUser = await User.findOne({email})
+        if(!validUser) return next(errorHandle(404, 'User not found!'));
+        const validPassword = bycryptjs.compareSync(password, validUser.password);
+        if(!validPassword) return next(errorHandle(401, 'Worng credentials!'));
+        const token = jwt.sign({id: validUser._id}, process.env.JWT_SECRET)
+        const { password: pass, ...rest} = validUser._doc;
+        res.cookie('access_token', token, {httpOnly: true}).status(200).json(rest)
+        //expires: new Date(Date.now() + 24*60*60*10)}
+
+
+    } catch (error) {
+        next(error)
+    }
+}
