23a1951b99e5e1b270feb5eca94769ae2ff723ae
Ridwanur Rashid Siam <siam.forcode01@gmail.com>
2024-01-27 07:15:09 +0600

    Completed sign in page functionality

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -28,8 +28,6 @@ export const signin = async (req, res, next) => {
         const { password: pass, ...rest} = validUser._doc;
         res.cookie('access_token', token, {httpOnly: true}).status(200).json(rest)
         //expires: new Date(Date.now() + 24*60*60*10)}
-
-
     } catch (error) {
         next(error)
     }
