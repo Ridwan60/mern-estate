7cf1bc3cabb4f88c63223954f4abab889e76fb6d
Hamim Rahman <hamimr3823@gmail.com>
2024-04-30 22:49:38 +0600

    Header Updated and Private Profile Page

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,12 +1,14 @@
-import {FaSearch} from 'react-icons/fa'
-import { Link } from 'react-router-dom'
+import { FaSearch } from "react-icons/fa";
+import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 export default function Header() {
+  const { currentUser } = useSelector((state) => state.user);
   return (
     <header className="bg-slate-200 shadow-md">
       <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
         <Link to="/">
-          <h1 className="font-bold text-sm sm: text-xl felx flex-wrap">
+          <h1 className="font-bold sm: text-xl flex flex-wrap">
             <span className="text-slate-500">Rashid</span>
             <span className="text-slate-700">Estate</span>
           </h1>
@@ -30,11 +32,19 @@ export default function Header() {
               About
             </li>
           </Link>
-          <Link to="/sign-in">
-            <li>Sign In</li>
+          <Link to="/profile">
+            {currentUser ? (
+              <img
+                className="rounded-full w-7 h-7 object-cover"
+                src={currentUser.avatar}
+                alt="profile"
+              />
+            ) : (
+              <li className="text-slate-700 hover:underline">Sign In</li>
+            )}
           </Link>
         </ul>
       </div>
     </header>
-  )
+  );
 }
