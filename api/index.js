ba30e0d69ec1bf063426cc8ee9350e45f4ca41b7
Hamim Rahman <hamimr3823@gmail.com>
2024-05-03 17:48:05 +0600

    Added Create Listing API

--- api/index.js
+++ api/index.js
@@ -1,36 +1,40 @@
-import dotenv from 'dotenv';
-import express, { json } from 'express';
-import mongoose from 'mongoose';
-import userRoute from './routes/user.route.js'
-import userAuth from "./routes/auth.route.js"
-import cookieParser from 'cookie-parser';
+import dotenv from "dotenv";
+import express, { json } from "express";
+import mongoose from "mongoose";
+import userRoute from "./routes/user.route.js";
+import userAuth from "./routes/auth.route.js";
+import listingRoute from "./routes/listing.route.js";
+import cookieParser from "cookie-parser";
 
 dotenv.config();
 
-mongoose.connect(process.env.MONGO).then(() =>{
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
     console.log("Conneced to MongoBD");
-}).catch((err) =>{
-    console.log("err")
-});
+  })
+  .catch((err) => {
+    console.log("err");
+  });
 
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
-app.listen(3000, ()=>{
-    console.log("Server is running on port 3000");
-    }
-);
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
 
-app.use('/api/user', userRoute);
-app.use('/api/auth', userAuth);
+app.use("/api/user", userRoute);
+app.use("/api/auth", userAuth);
+app.use("/api/listing", listingRoute);
 
 //middleware to handle errors
-app.use((err, req, res, next)=>{
-    const statusCode = err.statusCode || 500; //statusCode from the error
-    const message = err.message || 'Internal server error';
-    return res.status(statusCode).json({
-        success: false,
-        statusCode,
-        message,
-    })
-})
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500; //statusCode from the error
+  const message = err.message || "Internal server error";
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});
